25ccd97203dc03954838f87e2ccd7ae3f87d759b
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-03 19:15:53 +0000

    gettestcells

--- pytest_ipynb2/ipynb_parser.py
+++ pytest_ipynb2/ipynb_parser.py
@@ -9,5 +9,17 @@ def getcodecells(notebook: Path) -> dict[int, str]:
     rawcontents = notebook.read_text()
     contents = json.loads(rawcontents)
     return {
-        cellnr: "".join(cell["source"]) for cellnr, cell in enumerate(contents["cells"]) if cell["cell_type"] == "code"
+        cellnr: "".join(cell["source"])
+        for cellnr, cell in enumerate(contents["cells"])
+        if cell["cell_type"] == "code" and not cell["source"][0].startswith(r"%%ipytest")
+    }
+
+def gettestcells(notebook: Path) -> dict[int, str]:
+    """Return parsed test cells from a notebook. Identified by cell magic `%%ipytest`."""
+    rawcontents = notebook.read_text()
+    contents = json.loads(rawcontents)
+    return {
+        cellnr: "".join(cell["source"][1:]).strip()
+        for cellnr, cell in enumerate(contents["cells"])
+        if cell["cell_type"] == "code" and cell["source"][0].startswith(r"%%ipytest")
     }
--- tests/test_parsenotebook.py
+++ tests/test_parsenotebook.py
@@ -3,7 +3,7 @@ from textwrap import dedent
 
 import pytest
 
-from pytest_ipynb2 import getcodecells
+from pytest_ipynb2 import getcodecells, gettestcells
 
 
 @pytest.fixture
@@ -16,16 +16,25 @@ def testnotebook():
 def testnotebook_codecells(testnotebook: Path) -> dict:
     return getcodecells(testnotebook)
 
+@pytest.fixture
+def testnotebook_testcells(testnotebook: Path) -> dict:
+    return gettestcells(testnotebook)
+
 def test_codecells_number(testnotebook_codecells: dict):
     assert len(testnotebook_codecells) == 3
 
 def test_codecells_indexes(testnotebook_codecells: dict):
-    assert list(testnotebook_codecells.keys()) == [1,3,4]
+    assert list(testnotebook_codecells.keys()) == [1,3,5]
 
-def test_codecell4(testnotebook_codecells: dict):
+def test_testcells_number(testnotebook_testcells: dict):
+    assert len(testnotebook_testcells) == 1
+
+def test_testcells_indexes(testnotebook_testcells: dict):
+    assert list(testnotebook_testcells.keys()) == [4]
+
+
+def test_testcell_contents(testnotebook_testcells: dict):
     expected = dedent("""\
-        %%ipytest
-
         def test_adder():
             assert adder(1,2) == 3""")
-    assert testnotebook_codecells[4] == expected
+    assert testnotebook_testcells[4] == expected
