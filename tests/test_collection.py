71abb4e10aba7fd9685e53f2f8b12ef69d1bfd65
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-06 09:14:38 +0000

    assert one thing per testcase

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
@@ -23,12 +24,36 @@ def test_pytestersetup(example_dir: pytest.Pytester):
     expected_file = example_dir.path / "test_module.py"
     assert expected_file.exists(), str(list(example_dir.path.iterdir()))
 
-def test_collection(example_dir: pytest.Pytester):
+@dataclass
+class CollectedDir:
+    pytester_instance: pytest.Pytester
+    dir_node: pytest.Dir
+    items: list[pytest.Item]
+
+@pytest.fixture
+def collection_nodes(example_dir: pytest.Pytester) -> CollectedDir:
     dir_node = example_dir.getpathnode(example_dir.path)
-    assert type(dir_node) is pytest.Dir
-    collection_items = example_dir.genitems([dir_node])
-    assert len(collection_items) == 2
-    assert all(type(testcase) is pytest.Function for testcase in collection_items)
-    assert all(testcase.parent.name == "test_module.py" for testcase in collection_items)
-    assert all(type(testcase.parent) is pytest.Module for testcase in collection_items)
-    assert [testcase.name for testcase in collection_items] == ["test_adder", "test_globals"]
+    return CollectedDir(
+        pytester_instance=example_dir,
+        dir_node=dir_node,
+        items = example_dir.genitems([dir_node]),
+    )
+
+def test_collectedDir_type(collection_nodes: CollectedDir):
+    assert type(collection_nodes.dir_node) is pytest.Dir
+    
+def test_collectedItems_count(collection_nodes: CollectedDir):
+    assert len(collection_nodes.items) == 2
+
+def test_collectedItems_types(collection_nodes: CollectedDir):
+    assert all(type(testcase) is pytest.Function for testcase in collection_nodes.items)
+
+def test_collectedItems_names(collection_nodes: CollectedDir):
+    assert [testcase.name for testcase in collection_nodes.items] == ["test_adder", "test_globals"]
+
+def test_collectedItems_parents(collection_nodes: CollectedDir):
+    assert all(testcase.parent.name == "test_module.py" for testcase in collection_nodes.items)
+    assert all(type(testcase.parent) is pytest.Module for testcase in collection_nodes.items)
+
+def test_collection_depth(collection_nodes: CollectedDir):
+    assert all(testcase.parent.parent is collection_nodes.dir_node for testcase in collection_nodes.items)
