e801fb7d3abfd80076d82bc01c205486e2119ade
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-06 08:53:11 +0000

    collect the tests

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -7,7 +7,7 @@ import pytest
 def example_module() -> Path:
     """
     Location of example file.
-    
+
     As a fixture: using a constant causes resolution errors due to pytester messing with the path.
     If used directly in a test, fixture MUST be called BEFORE the pytester fixture to avoid path resolution issues.
     """
@@ -19,6 +19,14 @@ def example_dir(example_module: Path, pytester: pytest.Pytester) -> pytest.Pytes
     pytester.makepyfile(test_module=example_module.read_text())
     return pytester
 
-def test_collection(example_dir: pytest.Pytester):
+def test_pytestersetup(example_dir: pytest.Pytester):
     expected_file = example_dir.path / "test_module.py"
     assert expected_file.exists(), str(list(example_dir.path.iterdir()))
+
+def test_collection(example_dir: pytest.Pytester):
+    file_node = example_dir.getpathnode("test_module.py")
+    assert type(file_node) is pytest.Module
+    testcases = example_dir.genitems([file_node])
+    assert len(testcases) == 2
+    assert all(type(testcase) is pytest.Function for testcase in testcases)
+    assert [testcase.name for testcase in testcases] == ["test_adder", "test_globals"]
