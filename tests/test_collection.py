96f8d912713e072059534adf92f766f5ff802e3f
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-08 15:08:30 +0000

    check that from_dict() can cope with multiple modules

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -31,12 +31,14 @@ def test_pytestersetup(example_dir: pytest.Pytester):
     expected_file = example_dir.path / "test_module.py"
     assert expected_file.exists(), str(list(example_dir.path.iterdir()))
 
+
 @pytest.fixture
 def example_dir2files(example_module: Path, pytester: pytest.Pytester) -> pytest.Pytester:
     """The contents of `notebook.py` as `test_module.py` in an instantiated `Pytester` setup."""
     pytester.makepyfile(test_module=example_module.read_text())
     pytester.makepyfile(test_othermodule=example_module.read_text())
     return pytester
+
 
 def test_pytestersetup2files(example_dir2files: pytest.Pytester):
     expected_files = ["test_module.py", "test_othermodule.py"]
@@ -116,4 +118,33 @@ def test_collectiontree(expectedtree: CollectionTree, collection_nodes: Collecte
             <Module test_module.py>
                 <Function test_adder>
                 <Function test_globals>
+        """)
+
+
+@pytest.fixture
+def expectedtree_2files(example_dir2files: pytest.Pytester):
+    tree = {
+        (f"<Dir {example_dir2files.path.name}>", pytest.Dir): {
+            ("<Module test_module.py>", pytest.Module): {
+                ("<Function test_adder>", pytest.Function): None,
+                ("<Function test_globals>", pytest.Function): None,
+            },
+            ("<Module test_othermodule.py>", pytest.Module): {
+                ("<Function test_adder>", pytest.Function): None,
+                ("<Function test_globals>", pytest.Function): None,
+            },
+        },
+    }
+    return CollectionTree.from_dict(tree)
+
+
+def test_expectedtree_2files_repr(expectedtree_2files: CollectionTree, example_dir2files: pytest.Pytester):
+    assert repr(expectedtree_2files) == dedent(f"""\
+        <Dir {example_dir2files.path.name}> (<class '_pytest.main.Dir'>)
+            <Module test_module.py> (<class '_pytest.python.Module'>)
+                <Function test_adder> (<class '_pytest.python.Function'>)
+                <Function test_globals> (<class '_pytest.python.Function'>)
+            <Module test_othermodule.py> (<class '_pytest.python.Module'>)
+                <Function test_adder> (<class '_pytest.python.Function'>)
+                <Function test_globals> (<class '_pytest.python.Function'>)
         """)
