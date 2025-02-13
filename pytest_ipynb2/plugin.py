885f307fd285a5fbc160d90412020ee5a5d8c025
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-13 17:42:38 +0000

    correct type-hinting

--- pytest_ipynb2/plugin.py
+++ pytest_ipynb2/plugin.py
@@ -1,8 +1,13 @@
 """Actual plugin code."""
 
-from pathlib import Path
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 import pytest
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 
 class NotebookCollector(pytest.File):
@@ -11,7 +16,7 @@ class NotebookCollector(pytest.File):
     def collect(): ...  # noqa: ANN201, D102
 
 
-def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> NotebookCollector:
+def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> NotebookCollector | None:
     """Hook implementation to collect jupyter notebooks."""
     if file_path.suffix == ".ipynb":
         return NotebookCollector.from_parent(parent=parent, path=file_path)
