0d2ee7f27533ef78fdcebf1380c3f0771e9c2449
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-14 15:27:35 +0000

    extend tests to cover multiple notebooks

--- tests/test_runtests.py
+++ tests/test_runtests.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 
 @pytest.mark.parametrize(
-    ["example_dir","outcomes"],
+    ["example_dir", "outcomes"],
     [
         pytest.param(
             ExampleDir(
@@ -21,6 +21,29 @@ if TYPE_CHECKING:
                 "passed": 2,
             },
             id="Simple Notebook",
+        ),
+        pytest.param(
+            ExampleDir(
+                files=[Path("tests/assets/notebook_2tests.ipynb").absolute()],
+                conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+            ),
+            {
+                "passed": 3,
+            },
+            id="Notebook 2 test cells",
+        ),
+        pytest.param(
+            ExampleDir(
+                files=[
+                    Path("tests/assets/notebook_2tests.ipynb").absolute(),
+                    Path("tests/assets/notebook.ipynb").absolute(),
+                ],
+                conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+            ),
+            {
+                "passed": 5,
+            },
+            id="Both notebooks - unsorted",
         ),
     ],
     indirect=["example_dir"],
