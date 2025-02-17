e9dd18ae5c5959fd3ceb41e39ad92c7ea8f5cbe9
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-17 11:17:37 +0000

    knwon issue: stdout

--- tests/test_specialcases.py
+++ tests/test_specialcases.py
@@ -2,7 +2,7 @@
 Errors which only occur in these tests are likely due to the handling of the specific case, not basic functionality.
 
 TODO:
-- failing tests - Including Assertion rewriting
+- Assertion rewriting
 - tests using fixtures
 - parametrized tests
 """  # noqa: D405
@@ -20,7 +20,8 @@ passing_test = [
 failing_test = [
     r"%%ipytest",
     "def test_fails():",
-    "   assert False",
+    "    x = 1",
+    "    assert x == 2",
 ]
 
 test_cases = [
@@ -30,6 +31,7 @@ test_cases = [
             notebooks={"passing": passing_test},
         ),
         {"passed": 1},
+        None,
         id="Single Cell",
     ),
     pytest.param(
@@ -38,15 +40,34 @@ test_cases = [
             notebooks={"failing": failing_test},
         ),
         {"failed": 1},
+        [
+            "    def test_fails():",
+            "        x = 1",
+            ">       assert x == 2",
+            "E       assert 1 == 2",
+        ],
         id="Failing Test",
     ),
 ]
 
+
 @pytest.mark.parametrize(
-    ["example_dir", "outcomes"],
+    ["example_dir", "outcomes", "stdout"],
     test_cases,
     indirect=["example_dir"],
 )
-def test_complex_tests(example_dir: CollectedDir, outcomes: dict):
+def test_results(example_dir: CollectedDir, outcomes: dict, stdout: list[str]):  # noqa: ARG001
     results = example_dir.pytester_instance.runpytest()
     results.assert_outcomes(**outcomes)
+
+
+@pytest.mark.parametrize(
+    ["example_dir", "outcomes", "stdout"],
+    test_cases,
+    indirect=["example_dir"],
+)
+def test_output(example_dir: CollectedDir, outcomes: dict, stdout: list[str]):  # noqa: ARG001
+    results = example_dir.pytester_instance.runpytest()
+    if stdout:
+        pytest.xfail(reason="meaningful stdout NotImplemented")
+        assert results.stdout.fnmatch_lines(stdout, consecutive=True)
