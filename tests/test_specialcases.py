f187a3ee52af7e66f7ad1f6e4d557808bf176487
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-17 11:35:17 +0000

    improve test structure by consolidating parametrisation into custom mark

--- tests/test_specialcases.py
+++ tests/test_specialcases.py
@@ -2,10 +2,11 @@
 Errors which only occur in these tests are likely due to the handling of the specific case, not basic functionality.
 
 TODO:
-- Assertion rewriting
 - tests using fixtures
 - parametrized tests
 """  # noqa: D405
+
+from dataclasses import dataclass, field
 
 import pytest
 
@@ -24,50 +25,58 @@ failing_test = [
     "    assert x == 2",
 ]
 
-test_cases = [
-    pytest.param(
-        ExampleDir(
-            conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
-            notebooks={"passing": passing_test},
-        ),
-        {"passed": 1},
-        None,
-        id="Single Cell",
-    ),
-    pytest.param(
-        ExampleDir(
-            conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
-            notebooks={"failing": failing_test},
-        ),
-        {"failed": 1},
-        [
-            "    def test_fails():",
-            "        x = 1",
-            ">       assert x == 2",
-            "E       assert 1 == 2",
-        ],
-        id="Failing Test",
-    ),
-]
+
+@dataclass
+class ExpectedResults:
+    outcomes: dict[str, int]
+    """Dict of outcomes for https://docs.pytest.org/en/stable/reference/reference.html#pytest.RunResult.assert_outcomes"""
+    stdout: list[str] = field(default_factory=list)
+    """Consecutive lines expected in stdout"""
 
 
-@pytest.mark.parametrize(
-    ["example_dir", "outcomes", "stdout"],
-    test_cases,
+test_cases = pytest.mark.parametrize(
+    ["example_dir", "expected_results"],
+    [
+        pytest.param(
+            ExampleDir(
+                conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+                notebooks={"passing": passing_test},
+            ),
+            ExpectedResults(
+                outcomes={"passed": 1},
+            ),
+            id="Single Cell",
+        ),
+        pytest.param(
+            ExampleDir(
+                conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+                notebooks={"failing": failing_test},
+            ),
+            ExpectedResults(
+                outcomes={"failed": 1},
+                stdout=[
+                    "    def test_fails():",
+                    "        x = 1",
+                    ">       assert x == 2",
+                    "E       assert 1 == 2",
+                ],
+            ),
+            id="Failing Test",
+        ),
+    ],
     indirect=["example_dir"],
 )
-def test_results(example_dir: CollectedDir, outcomes: dict, stdout: list[str]):  # noqa: ARG001
-    results = example_dir.pytester_instance.runpytest()
-    results.assert_outcomes(**outcomes)
 
 
-@pytest.mark.parametrize(
-    ["example_dir", "outcomes", "stdout"],
-    test_cases,
-    indirect=["example_dir"],
-)
-def test_output(example_dir: CollectedDir, outcomes: dict, stdout: list[str]):  # noqa: ARG001
+@test_cases
+def test_results(example_dir: CollectedDir, expected_results: ExpectedResults):
     results = example_dir.pytester_instance.runpytest()
-    if stdout:
+    results.assert_outcomes(**expected_results.outcomes)
+
+
+@test_cases
+def test_output(example_dir: CollectedDir, expected_results: ExpectedResults):
+    results = example_dir.pytester_instance.runpytest()
+    if expected_results.stdout:
         pytest.xfail(reason="meaningful stdout NotImplemented")
-        assert results.stdout.fnmatch_lines(stdout, consecutive=True)
+        assert results.stdout.fnmatch_lines(expected_results.stdout, consecutive=True)
