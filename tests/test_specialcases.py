5f11f6a74c00b8a6112bf5114e01f5dd25ae8208
Mike Foster <i.am.mike.foster@gmail.com>
2025-02-17 10:47:57 +0000

    single cell

---
+++ tests/test_specialcases.py
@@ -0,0 +1,38 @@
+"""
+Errors which only occur in these tests are likely due to the handling of the specific case, not basic functionality.
+
+TODO:
+- failing tests - Including Assertion rewriting
+- tests using fixtures
+- parametrized tests
+"""  # noqa: D405
+
+import pytest
+
+from pytest_ipynb2.pytester_helpers import CollectedDir, ExampleDir
+
+passing_test = [
+    r"%%ipytest",
+    "def test_passes():",
+    "   assert True",
+]
+
+test_cases = [
+    pytest.param(
+        ExampleDir(
+            conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+            notebooks={"passing": passing_test},
+        ),
+        {"passed": 1},
+        id="Single Cell",
+    ),
+]
+
+@pytest.mark.parametrize(
+    ["example_dir", "outcomes"],
+    test_cases,
+    indirect=["example_dir"],
+)
+def test_complex_tests(example_dir: CollectedDir, outcomes: dict):
+    results = example_dir.pytester_instance.runpytest()
+    results.assert_outcomes(**outcomes)
