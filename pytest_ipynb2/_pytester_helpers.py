41b53c9e506dbf9335b87b07b4b087f8f8da6ad3
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-01 11:15:24 +0000

    explain use of next(iter(request.keywords))

--- pytest_ipynb2/_pytester_helpers.py
+++ pytest_ipynb2/_pytester_helpers.py
@@ -269,6 +269,7 @@ def example_dir(
             nbformat.write(nb=nbnode, fp=pytester.path / f"{notebook}.ipynb")
         cached_dir = example_dir_cache[example] = ExampleDir(pytester=pytester)
     else:
+        # 1st keyword is the test name (incl. any parametrized id)
         msg = f"Using cached {cached_dir.path} for {next(iter(request.keywords))}"
         warn(msg, stacklevel=1)
     return example_dir_cache[example]
