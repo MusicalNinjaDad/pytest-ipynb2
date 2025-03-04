c4b87a31e325e20d588daad0a573849d38fcd591
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-04 14:39:25 +0000

    add failing test for running specific cell from command line

---
+++ tests/test_commandline.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import pytest
+
+from pytest_ipynb2._pytester_helpers import ExampleDir, ExampleDirSpec
+
+
+@pytest.mark.parametrize(
+    "example_dir",
+    [
+        ExampleDirSpec(
+            files=[Path("tests/assets/notebook_2tests.ipynb").absolute()],
+            conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+        ),
+    ],
+    indirect=True,
+)
+def test_runnotebook(example_dir: ExampleDir):
+    result = example_dir.pytester.runpytest("notebook_2tests.ipynb")
+    result.assert_outcomes(passed=3)
+
+@pytest.mark.parametrize(
+    "example_dir",
+    [
+        ExampleDirSpec(
+            files=[Path("tests/assets/notebook_2tests.ipynb").absolute()],
+            conftest="pytest_plugins = ['pytest_ipynb2.plugin']",
+        ),
+    ],
+    indirect=True,
+)
+def test_cell(example_dir: ExampleDir):
+    result = example_dir.pytester.runpytest("<notebook_2tests.ipynb[Cell4]>")
+    result.assert_outcomes(passed=2)
