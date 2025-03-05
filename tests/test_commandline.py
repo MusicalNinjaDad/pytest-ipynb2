a1cefacba9d9a6eaba4a03df62975d86e63ebe2e
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-05 19:37:57 +0000

    fix #49

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -10,12 +10,13 @@ from pytest_ipynb2._pytester_helpers import ExampleDir, ExampleDirSpec
     [
         ExampleDirSpec(
             files=[Path("tests/assets/notebook_2tests.ipynb").absolute()],
+            args=["notebook_2tests.ipynb"],
         ),
     ],
     indirect=True,
 )
 def test_runnotebook(example_dir: ExampleDir):
-    result = example_dir.pytester.runpytest("notebook_2tests.ipynb")
+    result = example_dir.runresult
     result.assert_outcomes(passed=3)
 
 
@@ -24,39 +25,11 @@ def test_runnotebook(example_dir: ExampleDir):
     [
         ExampleDirSpec(
             files=[Path("tests/assets/notebook_2tests.ipynb").absolute()],
+            args=["<notebook_2tests.ipynb[Cell4]>"],
         ),
     ],
     indirect=True,
 )
 def test_cell(example_dir: ExampleDir):
-    # TODO: #49 (can't rerun runpytest)
-    result = example_dir.pytester.runpytest("<notebook_2tests.ipynb[Cell4]>")
+    result = example_dir.runresult
     result.assert_outcomes(passed=2)
-
-@pytest.mark.parametrize(
-    "example_dir",
-    [
-        ExampleDirSpec(
-            files=[Path("tests/assets/test_module.py").absolute()],
-        ),
-    ],
-    indirect=True,
-)
-def test_module(example_dir: ExampleDir):
-    result = example_dir.pytester.runpytest("test_module.py")
-    result.assert_outcomes(passed=2)
-
-
-@pytest.mark.parametrize(
-    "example_dir",
-    [
-        ExampleDirSpec(
-            files=[Path("tests/assets/test_module.py").absolute()],
-        ),
-    ],
-    indirect=True,
-)
-def test_func(example_dir: ExampleDir):
-    # TODO: #49 (can't rerun runpytest)
-    result = example_dir.pytester.runpytest("test_module.py::test_globals")
-    result.assert_outcomes(passed=1)
