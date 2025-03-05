baea77145ba3242202f418b234aa2a53fc31b7df
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-05 19:28:25 +0000

    #49 also occurs for std python tests ... so is a fixture issue, not a collector issue

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -32,3 +32,31 @@ def test_cell(example_dir: ExampleDir):
     # TODO: #49 (can't rerun runpytest)
     result = example_dir.pytester.runpytest("<notebook_2tests.ipynb[Cell4]>")
     result.assert_outcomes(passed=2)
+
+@pytest.mark.parametrize(
+    "example_dir",
+    [
+        ExampleDirSpec(
+            files=[Path("tests/assets/test_module.py").absolute()],
+        ),
+    ],
+    indirect=True,
+)
+def test_module(example_dir: ExampleDir):
+    result = example_dir.pytester.runpytest("test_module.py")
+    result.assert_outcomes(passed=2)
+
+
+@pytest.mark.parametrize(
+    "example_dir",
+    [
+        ExampleDirSpec(
+            files=[Path("tests/assets/test_module.py").absolute()],
+        ),
+    ],
+    indirect=True,
+)
+def test_func(example_dir: ExampleDir):
+    # TODO: #49 (can't rerun runpytest)
+    result = example_dir.pytester.runpytest("test_module.py::test_globals")
+    result.assert_outcomes(passed=1)
