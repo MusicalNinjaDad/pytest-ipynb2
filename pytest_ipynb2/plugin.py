28b74df1b040faaad02cec569bf36cfc8f3f701a
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-08 13:01:15 +0000

    reorder plugin.py for logical readability

--- pytest_ipynb2/plugin.py
+++ pytest_ipynb2/plugin.py
@@ -48,6 +48,153 @@ ipynb2_monkeypatches = pytest.StashKey[dict[tuple[ModuleType, str], FunctionType
 CELL_PREFIX: Final[str] = "Cell"
 
 
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_load_initial_conftests(early_config, parser, args: list[str]) -> Generator[None, None, None]:  # noqa: ANN001, ARG001
+    """
+    Convert any CellPaths passed as commandline args to "::"-separated nodeids.
+
+    Even though we are using `path/to/notebook.ipynb[Celln]::test_func` as nodeid format, pytest will still accept
+    `path/to/notebook.ipynb::Celln::test_func` as a valid nodeid. For unknown reasons,
+    `_pytest.main.resolve_collection_argument` removes anything after the first `[`
+    """
+    # TODO: #50 handle `path/to/notebook.ipynb[Celln]::test_func` in `_pytest.main.resolve_collection_argument`
+    # Relying on pytest accepting `path/to/notebook.ipynb::Celln::test_func` as a valid nodeid may be a source of
+    # future bugs.
+
+    for idx, arg in enumerate(args):
+        if CellPath.is_cellpath(arg.split("::")[0]):
+            args[idx] = CellPath.to_nodeid(arg)
+    yield
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)  # ensure exeution order before any other plugins
+def pytest_sessionstart(session: pytest.Session) -> Generator[None, None, None]:
+    """Monkeypatch pytest to handle CellPath, store patches in stash to revert later."""
+    session.stash[ipynb2_monkeypatches] = CellPath.patch_pytest_absolutepath()
+    yield
+
+
+def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Notebook | None:
+    """Hook implementation to collect jupyter notebooks."""
+    if file_path.suffix == ".ipynb":
+        nodeid = os.fspath(file_path.relative_to(parent.config.rootpath))
+        return Notebook.from_parent(parent=parent, path=file_path, nodeid=nodeid)
+    return None
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)  # ensure exeution order after any other plugins
+def pytest_sessionfinish(session: pytest.Session, exitstatus: int | pytest.ExitCode) -> Generator[None, None, None]:  # noqa: ARG001
+    """Revert Monkeypatches based on stashed versions."""
+    yield
+    for (module, attr), orig in session.stash[ipynb2_monkeypatches].items():
+        setattr(module, attr, orig)
+
+
+class Notebook(pytest.File):
+    """A collector for jupyter notebooks."""
+
+    def collect(self) -> Generator[Cell, None, None]:
+        """Yield `Cell`s for all cells which contain tests."""
+        parsed = _ParsedNotebook(self.path)
+        for testcellid in parsed.muggled_testcells.ids():
+            name = f"{CELL_PREFIX}{testcellid}"
+            nodeid = f"{self.nodeid}[{name}]"
+            cell = Cell.from_parent(
+                parent=self,
+                name=name,
+                nodeid=nodeid,
+                path=CellPath(f"{self.path}[{name}]"),
+            )
+            cell.stash[ipynb2_notebook] = parsed
+            cell.stash[ipynb2_cellid] = testcellid
+            yield cell
+
+
+class IpynbItemMixin:
+    """Provides various overrides to handle our pseudo-path."""
+
+    path: CellPath
+    name: str
+
+    def reportinfo(self) -> tuple[Path, int, str]:
+        """
+        Returns tuple of notebook path, (linenumber=)0, Celln::testname.
+
+        `reportinfo` is used by `location` and included as the header line in the report:
+            ```
+            ==== FAILURES ====
+            ___ reportinfo[2] ___
+            ```
+        """
+        # `nodes.Item.location` calls `absolutepath()` and then `main._node_location_to_relpath` which caches the
+        # results in `_bestrelpathcache[node_path]` very early in the test process.
+        # As we provide the full CellPath as reportinfo[0] we need to patch `_pytest.nodes.absolutepath` in
+        # `CellPath.patch_pytest_pathlib` (above)
+        #
+        # `TerminalReporter._locationline` adds a `<-` section if `nodeid.split("::")[0] != location[0]`.
+        # Verbosity<2 tests runs are grouped by location[0] in the testlog.
+        return self.path, 0, f"{self.path.cell}::{self.name}"
+
+
+class Cell(IpynbItemMixin, pytest.Module):
+    """
+    A collector for jupyter notebook cells.
+
+    `pytest` will recognise these cells as `pytest.Module`s and use standard collection on them as it would any other
+    python module.
+    """
+
+    def collect(self) -> Generator[pytest.Function, None, None]:
+        """Rebless children to include our overrides from the Mixin."""
+        # TODO(MusicalNinjaDad): #22 Handle Tests grouped in Class
+        for item in super().collect():
+            item_type = type(item)
+            type_with_mixin = types.new_class(item_type.__name__, (IpynbItemMixin, item_type))
+            item.__class__ = type_with_mixin
+            yield item
+
+    def __repr__(self) -> str:
+        """Don't duplicate the word "Cell" in the repr."""
+        return f"<{type(self).__name__} {self.stash[ipynb2_cellid]}>"
+
+    def _getobj(self) -> ModuleType:
+        """
+        The main magic.
+
+        - loads the cell's source
+        - applies assertion rewriting
+        - creates a pseudo-module for the cell, with a pseudo-filename
+        - executes all non-test code cells above inside the pseudo-module.__dict__
+        - then executes the test cell inside the pseudo-module.__dict__
+        - finally adds the test cell to the linecache so that inspect can find the source
+        """
+        notebook = self.stash[ipynb2_notebook]
+        cellid = self.stash[ipynb2_cellid]
+
+        cellsabove = [str(cellsource) for cellsource in notebook.muggled_codecells[:cellid]]
+        testcell_source = str(notebook.muggled_testcells[cellid])
+
+        cell_filename = str(self.path)
+
+        testcell_ast = ast.parse(testcell_source, filename=cell_filename)
+        _pytest.assertion.rewrite.rewrite_asserts(
+            mod=testcell_ast,
+            source=bytes(testcell_source, encoding="utf-8"),
+            module_path=str(self.path),
+            config=self.config,
+        )
+
+        testcell = compile(testcell_ast, filename=cell_filename, mode="exec")
+
+        dummy_spec = importlib.util.spec_from_loader(f"{self.name}", loader=None)
+        dummy_module = importlib.util.module_from_spec(dummy_spec)
+        for cell in cellsabove:
+            exec(cell, dummy_module.__dict__)  # noqa: S102
+        exec(testcell, dummy_module.__dict__)  # noqa: S102
+        linecache.cache[cell_filename] = (0, None, testcell_source.splitlines(keepends=True), cell_filename)
+        return dummy_module
+
+
 class CellPath(Path):
     """Provide handling of Cells specified as `path/to/file[Celln]`."""
 
@@ -146,150 +293,3 @@ class CellPath(Path):
         _pytest.nodes.absolutepath = _absolutepath
 
         return original_functions
-
-
-class IpynbItemMixin:
-    """Provides various overrides to handle our pseudo-path."""
-
-    path: CellPath
-    name: str
-
-    def reportinfo(self) -> tuple[Path, int, str]:
-        """
-        Returns tuple of notebook path, (linenumber=)0, Celln::testname.
-
-        `reportinfo` is used by `location` and included as the header line in the report:
-            ```
-            ==== FAILURES ====
-            ___ reportinfo[2] ___
-            ```
-        """
-        # `nodes.Item.location` calls `absolutepath()` and then `main._node_location_to_relpath` which caches the
-        # results in `_bestrelpathcache[node_path]` very early in the test process.
-        # As we provide the full CellPath as reportinfo[0] we need to patch `_pytest.nodes.absolutepath` in
-        # `CellPath.patch_pytest_pathlib` (above)
-        #
-        # `TerminalReporter._locationline` adds a `<-` section if `nodeid.split("::")[0] != location[0]`.
-        # Verbosity<2 tests runs are grouped by location[0] in the testlog.
-        return self.path, 0, f"{self.path.cell}::{self.name}"
-
-
-class Notebook(pytest.File):
-    """A collector for jupyter notebooks."""
-
-    def collect(self) -> Generator[Cell, None, None]:
-        """Yield `Cell`s for all cells which contain tests."""
-        parsed = _ParsedNotebook(self.path)
-        for testcellid in parsed.muggled_testcells.ids():
-            name = f"{CELL_PREFIX}{testcellid}"
-            nodeid = f"{self.nodeid}[{name}]"
-            cell = Cell.from_parent(
-                parent=self,
-                name=name,
-                nodeid=nodeid,
-                path=CellPath(f"{self.path}[{name}]"),
-            )
-            cell.stash[ipynb2_notebook] = parsed
-            cell.stash[ipynb2_cellid] = testcellid
-            yield cell
-
-
-class Cell(IpynbItemMixin, pytest.Module):
-    """
-    A collector for jupyter notebook cells.
-
-    `pytest` will recognise these cells as `pytest.Module`s and use standard collection on them as it would any other
-    python module.
-    """
-
-    def __repr__(self) -> str:
-        """Don't duplicate the word "Cell" in the repr."""
-        return f"<{type(self).__name__} {self.stash[ipynb2_cellid]}>"
-
-    def _getobj(self) -> ModuleType:
-        """
-        The main magic.
-
-        - loads the cell's source
-        - applies assertion rewriting
-        - creates a pseudo-module for the cell, with a pseudo-filename
-        - executes all non-test code cells above inside the pseudo-module.__dict__
-        - then executes the test cell inside the pseudo-module.__dict__
-        - finally adds the test cell to the linecache so that inspect can find the source
-        """
-        notebook = self.stash[ipynb2_notebook]
-        cellid = self.stash[ipynb2_cellid]
-
-        cellsabove = [str(cellsource) for cellsource in notebook.muggled_codecells[:cellid]]
-        testcell_source = str(notebook.muggled_testcells[cellid])
-
-        cell_filename = str(self.path)
-
-        testcell_ast = ast.parse(testcell_source, filename=cell_filename)
-        _pytest.assertion.rewrite.rewrite_asserts(
-            mod=testcell_ast,
-            source=bytes(testcell_source, encoding="utf-8"),
-            module_path=str(self.path),
-            config=self.config,
-        )
-
-        testcell = compile(testcell_ast, filename=cell_filename, mode="exec")
-
-        dummy_spec = importlib.util.spec_from_loader(f"{self.name}", loader=None)
-        dummy_module = importlib.util.module_from_spec(dummy_spec)
-        for cell in cellsabove:
-            exec(cell, dummy_module.__dict__)  # noqa: S102
-        exec(testcell, dummy_module.__dict__)  # noqa: S102
-        linecache.cache[cell_filename] = (0, None, testcell_source.splitlines(keepends=True), cell_filename)
-        return dummy_module
-
-    def collect(self) -> Generator[pytest.Function, None, None]:
-        """Rebless children to include our overrides from the Mixin."""
-        # TODO(MusicalNinjaDad): #22 Handle Tests grouped in Class
-        for item in super().collect():
-            item_type = type(item)
-            type_with_mixin = types.new_class(item_type.__name__, (IpynbItemMixin, item_type))
-            item.__class__ = type_with_mixin
-            yield item
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)  # ensure exeution order before any other plugins
-def pytest_sessionstart(session: pytest.Session) -> Generator[None, None, None]:
-    """Monkeypatch pytest to handle CellPath, store patches in stash to revert later."""
-    session.stash[ipynb2_monkeypatches] = CellPath.patch_pytest_absolutepath()
-    yield
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)  # ensure exeution order after any other plugins
-def pytest_sessionfinish(session: pytest.Session, exitstatus: int | pytest.ExitCode) -> Generator[None, None, None]:  # noqa: ARG001
-    """Revert Monkeypatches based on stashed versions."""
-    yield
-    for (module, attr), orig in session.stash[ipynb2_monkeypatches].items():
-        setattr(module, attr, orig)
-
-
-def pytest_collect_file(file_path: Path, parent: pytest.Collector) -> Notebook | None:
-    """Hook implementation to collect jupyter notebooks."""
-    if file_path.suffix == ".ipynb":
-        nodeid = os.fspath(file_path.relative_to(parent.config.rootpath))
-        return Notebook.from_parent(parent=parent, path=file_path, nodeid=nodeid)
-    return None
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_load_initial_conftests(early_config, parser, args: list[str]) -> Generator[None, None, None]:  # noqa: ANN001, ARG001
-    """
-    Convert any CellPaths passed as commandline args to "::"-separated nodeids.
-
-    Even though we are using `path/to/notebook.ipynb[Celln]::test_func` as nodeid format, pytest will still accept
-    `path/to/notebook.ipynb::Celln::test_func` as a valid nodeid. For unknown reasons,
-    `_pytest.main.resolve_collection_argument` removes anything after the first `[`
-    """
-    # TODO: #50 handle `path/to/notebook.ipynb[Celln]::test_func` in `_pytest.main.resolve_collection_argument`
-    # Relying on pytest accepting `path/to/notebook.ipynb::Celln::test_func` as a valid nodeid may be a source of
-    # future bugs.
-
-    for idx, arg in enumerate(args):
-        if CellPath.is_cellpath(arg.split("::")[0]):
-            args[idx] = CellPath.to_nodeid(arg)
-    yield
